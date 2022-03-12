4c005e81d0c8c5e491d3fe5e10dfa755fb06db8f
nehalem <nehalem@firemail.cc>
2022-03-12 12:06:46 +0100

    v0.0.2 rewrite

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,20 +1,5 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
-
+@SpringBootTest
 public class SampleTest {
-    final private Logger logger = LoggerFactory.getLogger(SampleTest.class);
-    @Test
-    public void test1() {
-        var l = mock(List.class);
-        logger.info("Jeboce?");
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            throw new RuntimeException();
-        });
-    }
 }
